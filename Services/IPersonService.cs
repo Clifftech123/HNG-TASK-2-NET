35882cb65baa42f290543b5def5fd77930f0b5c5
Benjamin Aduo <manizza14@gmail.com>
2023-09-24 16:23:17 +0000

    small refinement

---
+++ Services/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using HNG_Task_2_NET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNG_Task_2_NET.Services
+{
+    public interface IPersonService
+    {
+        Task<List<Person>> GetPersonsAsync();
+        Task<Person> GetPersonAsync(int id);
+        Task<Person> CreatePersonAsync(Person person);
+        Task<Person> UpdatePersonAsync(int id, Person person);
+        Task<Person> DeletePersonAsync(int id);
+        Task<Person> GetPersonByNameAsync(string name);
+
+    }
+}
